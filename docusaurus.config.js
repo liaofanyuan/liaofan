e2c9e935026818104b7349568aa930c44308f829
liaofan <liaofany@gmail.com>
2024-04-21 09:27:50 +0800

    update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,12 +8,12 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Liaofan\'s 4 lessons ',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://yuanliaofan.xyz',
+  url: 'http://yuanliaofan.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
